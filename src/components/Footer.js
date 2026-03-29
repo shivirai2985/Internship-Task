1f9842128e51a9ef020c4496240a4a7d57d96c4a
Shivangi Ray <shivirai.2985@gmail.com>
2026-03-29 22:54:34 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+function Footer() {
+    return ( 
+    
+    <div className = "footer" >
+
+        <div className = "footer-container" >
+        <div className = "footer-box" >
+        <h3> 🏢Construction Company Pvt.Ltd. </h3>
+         <p> We build strong and modern infrastructure with quality and trust. </p> 
+         </div >
+
+        <div className = "footer-box" >
+        <h3 > Quick Links </h3> 
+        <p> Home </p>
+         <p> About </p>
+          <p> Services </p> 
+          <p> Projects </p> 
+          </div >
+
+        <div className = "footer-box" >
+        <h3 > Contact </h3> 
+        <p> Main Road, Sagar, Madhya Pradesh </p>
+         <p> 📞+91 XXXXX XXXXX </p>  
+         <p> ✉ info @constructions.com </p>  
+         </div>
+          </div>
+
+        <div className = "footer-bottom" >
+
+        <p> ©2026 Construction Company Pvt.Ltd. | All Right Reserved </p> 
+        </div>
+         </div>
+    );
+}
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+function Navbar() {
+    return ( 
+        <div className="navbar">
+        <h2>
+        BuildCorp
+        </h2> 
+        <ul>
+            <li>Home</li>
+            <li> About</li>
+            <li>Services</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+        </div>
+    );
+}
+export default Navbar;
